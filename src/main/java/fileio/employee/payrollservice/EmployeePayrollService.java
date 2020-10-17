3c4eebc217e48a23f71aee3000b3450a3f1f3efd
Mukesh Ranjan <mukeshranjan674@gmail.com>
2020-10-17 15:57:00 +0530

    [Mukesh] add : Ability to Read and Write Employee Payroll to a Console

--- src/main/java/fileio/employee/payrollservice/EmployeePayrollService.java
+++ src/main/java/fileio/employee/payrollservice/EmployeePayrollService.java
@@ -1,5 +1,39 @@
 package fileio.employee.payrollservice;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 public class EmployeePayrollService {
 
+	public enum IOService{CONSOLE_IO, FILE_IO, REST_IO}
+	private List<EmployeePayrollData> employeePayrollList;
+	
+	public EmployeePayrollService() {
+		employeePayrollList = new ArrayList<EmployeePayrollData>();
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+		Scanner consoleInputReader = new Scanner(System.in);
+		employeePayrollService.readEmployeePayrollData(consoleInputReader);
+		employeePayrollService.writeEmployeePayrollData();
+	}
+	
+	private void readEmployeePayrollData(Scanner consoleInputReader) {
+		System.out.println("Enter Employee ID: ");
+		int id = consoleInputReader.nextInt();
+		System.out.println("Enter Employee name: ");
+		String name = consoleInputReader.next();
+		System.out.println("Enter Employee Salary: ");
+		double salary = consoleInputReader.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
+	}
+	
+	private void writeEmployeePayrollData() {
+		System.out.println("\nWriting Employee Payroll Data to Console\n" + employeePayrollList);
+	}
 }
